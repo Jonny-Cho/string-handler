27f80cbd3a6214c7184e975b41f12223345d93c6
Jonny-Cho <andante2183@gmail.com>
2022-03-13 14:28:12 +0900

    setting: 컨트롤러, 서비스 세팅

---
+++ src/main/java/com/handler/stringhandler/dto/ParseResponse.java
@@ -0,0 +1,9 @@
+package com.handler.stringhandler.dto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ParseResponse {
+    private final String quotient;
+    private final String remainder;
+}
