d6e92022a3c02642f74ea38ef7abceec8480f160
Jonny-Cho <andante2183@gmail.com>
2022-03-13 15:08:53 +0900

    feat: service에 separate 적용

--- src/main/java/com/handler/parse/service/ParseService.java
+++ src/main/java/com/handler/parse/service/ParseService.java
@@ -2,6 +2,7 @@ package com.handler.parse.service;
 
 import com.handler.parse.dto.ParseRequest;
 import com.handler.parse.dto.ParseResponse;
+import com.handler.parse.util.Separator;
 import com.handler.parse.util.UrlConnector;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,12 @@ import org.springframework.stereotype.Service;
 public class ParseService {
 
     private final UrlConnector urlConnector;
+    private final Separator separator;
 
     public ParseResponse parse(final ParseRequest request) {
         final String html = urlConnector.getHtml(request.getUrl());
         final String exposedHtml = request.getExposureType().getExposedHtml(html);
+        final Separator separate = separator.separate(exposedHtml);
 
         return null;
     }
