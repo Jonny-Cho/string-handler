11c2a315a612bee55e99c3fae967903da9636736
Jonny-Cho <andante2183@gmail.com>
2022-03-13 14:13:04 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/com/handler/stringhandler/StringHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.handler.stringhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringHandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/handler/stringhandler/StringHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handler.stringhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
